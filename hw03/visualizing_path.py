ffb49e9c7a363e099503c7fc5ff11664f55f794e
Maxym Zviahin <maxzvyagin@gmail.com>
2025-05-05 23:56:13 +0300

    redo_all_homework_added_comments

--- hw03/visualizing_path.py
+++ hw03/visualizing_path.py
@@ -1,29 +1,30 @@
-import sys
-from pathlib import Path
-import colorama
+import sys  #Імпортуєм модуль sys для роботи з аргументами коммандоного рядка
+from pathlib import Path #Імпортуємо клас Pathз модуля pathlib для роботи з шляхами
+import colorama #Імпортуємо модуль для кольорового виводу в термінал
 
-
+#Сортувальна функція
 def sort_function(item):
-      return (item.is_file(), item.name.lower())
+      return (item.is_file(), item.name.lower()) #Сортує елементи за типом (файл чи папка) та за назвою в нижньому регістрі
 
+#Функція для візуалізації шляху до директороії з кольоровим виводом
 def visualize_directory(directory_path: str, indent: int = 0):
-    directory_path = Path(directory_path).resolve()
-    if not directory_path.exists() or not directory_path.is_dir():
-            return colorama.Fore.RED + f'Папка {directory_path} не існує.' + colorama.Style.RESET_ALL
-    print(colorama.Fore.CYAN + f'\n{directory_path}' + colorama.Style.RESET_ALL)
+    directory_path = Path(directory_path).resolve() #Перетворює рядок у об'єкт Path та отримує абсолютний шлях
+    if not directory_path.exists() or not directory_path.is_dir(): #Перевіряє чи існує директорія та чи є вона папкою
+            return colorama.Fore.RED + f'Папка {directory_path} не існує.' + colorama.Style.RESET_ALL #Якщо не існує, повертає повідомлення про помилку
+    for item in sorted(directory_path.iterdir(), key=sort_function): #Перебирає всі елементи в директорії, сортує їх за допомогою функції sort_function
+        prefix = "  " * indent #Створює префікс для відступу
+        if item.is_dir(): #Якщо елемент є директорією
+            print(prefix + colorama.Fore.BLUE + f'{item.name}' + colorama.Style.RESET_ALL) #Виводить назву директорії
+            visualize_directory(item, indent + 1) #Рекурсивно викликає функцію для візуалізації вмісту директоріїї
+        else: 
+            print(prefix + colorama.Fore.GREEN + f'{item.name}' + colorama.Style.RESET_ALL) #Якщо елемент є файлом, виводить його назву
 
-    for item in sorted(directory_path.iterdir(), key=sort_function):
-        prefix = "  " * indent
-        if item.is_dir():
-            print(prefix + colorama.Fore.BLUE + f'{item.name}' + colorama.Style.RESET_ALL)
-            visualize_directory(item, indent + 1)
-        else:
-            print(prefix + colorama.Fore.GREEN + f'{item.name}' + colorama.Style.RESET_ALL)
+
 if __name__ =='__main__':
-    if len(sys.argv) != 2:
-        print(colorama.Fore.RED + "Використання: visualizing_path.py <шлях_до_директорії>" + colorama.Fore.RESET)
+    if len(sys.argv) != 2: #Перевіряє чи передано правильну кількість аргументів
+        print(colorama.Fore.RED + "Використання: visualizing_path.py <шлях_до_директорії>" + colorama.Fore.RESET) #Виводить повідомлення про помилку
     else:
-     visualize_directory(sys.argv[1])
+     visualize_directory(sys.argv[1]) #Викликає функцію для візуалізації директорії, передаючи шлях з аргументів командного рядка
           
         
         
